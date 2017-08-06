464a446f894e7886213f66c7f592aaf9b2a492ac
Josh Marlow <joshmarlow@gmail.com>
2017-08-05 21:30:12 -0400

    Cleaned up utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,5 @@
+//! Internal utilities
+
 use itertools::FoldWhile::{Continue, Done};
 
 use itertools::Itertools;
@@ -21,39 +23,4 @@ pub fn fold_while_some<A, E>(init_acc: A, iter: &mut Iterator<Item = E>, f: &Fn(
         Some(value) => Continue(Some(value)),
         None => Done(None),
     })
-}
-
-pub fn fold_while_ok<A, I, E: std::fmt::Debug>(init_acc: A, iter: &mut Iterator<Item = I>, f: &Fn(A, I) -> Result<A, E>) -> Result<A, E> {
-    iter.fold_while(Ok(init_acc), |acc, x| match f(acc.unwrap(), x) {
-        Ok(value) => Continue(Ok(value)),
-        Err(err) => Done(Err(err)),
-    })
-}
-
-pub fn display_graph(rendering: String, base_filename: String) {
-    use std::fs::File;
-    use std::io::prelude::*;
-    use std::path::Path;
-    use std::process::Command;
-
-    let dot_filepath = format!("{}.dot", base_filename);
-    let png_filepath = format!("{}.png", dot_filepath);
-    File::create(&Path::new(&dot_filepath))
-        .and_then(|mut file| file.write_all(rendering.as_str().as_bytes()))
-        .and_then(|_| {
-            Command::new("dot")
-                .arg("-Tpng")
-                .arg(dot_filepath.as_str())
-                .arg("-O")
-                .output()
-                .or_else(|err| panic!("{:?}", err))
-                .and_then(|_| {
-                    Command::new("open")
-                        .arg(png_filepath.as_str())
-                        .output()
-                        .or_else(|err| panic!("{:?}", err))
-                })
-                .and_then(|_| Ok(()))
-        })
-        .unwrap()
 }
